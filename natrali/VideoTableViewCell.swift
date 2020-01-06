b62acadc506130ed93949ee162ea3ae9a59b859d
anikamorris <anikamorris11@gmail.com>
2020-01-06 12:44:12 -0800

    set up table view cells and connect outlets

---
+++ natrali/VideoTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  VideoTableViewCell.swift
+//  natrali
+//
+//  Created by Anika Morris on 1/6/20.
+//  Copyright © 2020 Anika Morris. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var videoImage: UIImageView!
+    @IBOutlet weak var videoTitle: UILabel!
+    
+}
